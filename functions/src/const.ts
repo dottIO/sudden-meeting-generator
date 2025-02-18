01c307f0006be61f42d3b94787389e8f396cddd9
Haruya <h.nakamura@thedott.io>
2025-02-18 09:37:05 +0900

    不要な定数の削除

--- functions/src/const.ts
+++ functions/src/const.ts
@@ -3,11 +3,6 @@ export const SLACK_API = {
   CHAT_POST_MESSAGE: "https://slack.com/api/chat.postMessage",
 };
 
-export const GOOGLE_CALENDAR_API = {
-  TOKEN: "https://oauth2.googleapis.com/token",
-};
-
 export const REGION = "asia-northeast1";
 export const SLACK_REDIRECT_URI = `https://${REGION}-sudden-meeting-generator.cloudfunctions.net/slack-callback`;
 export const SLACK_BOT_TOKEN = process.env.SLACK_BOT_TOKEN || "";
-export const SLACK_TARGET_CHANNEL = process.env.SLACK_TARGET_CHANNEL || "";
